ace285cc61850451a490b787cd7bc90c64c304dd
Diane <d.bunod@gmail.com>
2018-08-21 18:39:43 +0200

    last version

---
+++ README.md
@@ -0,0 +1,15 @@
+# README
+
+## Startup FoodTech
+
+Cette app Ruby on rails est la landing page d'un restaurant gastronomique de kebab. Elle a été conçue par Diane.
+
+L'intérêt de l'app est l'utilisation de Javascript et plus particulièrement JQuery. Il y a un carousel d'images ainsi qu'une tab qui permet d'afficher sur une même page la présentation, le menu et l'itinéraire du restaurant.
+
+## Comment démarrer ?
+
+Il suffit de cliquer sur ce lien pour voir l'app en production : https://startup-foodtech-diane.herokuapp.com/.
+
+Sinon, il suffit de clone ou download le dossier et de run bundle install.
+
+Les versions de Ruby et Rails doivent être respectivement 2.5.1 et 5.2.1.
---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,79 @@
+
+$(function() {
+
+    $('.tableau .tabs li').on('click', function() {
+
+        var $panel = $(this).closest('.tableau');
+
+        $panel.find('.tabs li.active').removeClass('active');
+        $(this).addClass('active');
+
+        //figure out which panel to show
+        var panelToShow = $(this).attr('rel');
+
+        //hide current panel
+        $panel.find('.panel.active').slideUp(300, showNextPanel);
+
+        //show next panel
+        function showNextPanel() {
+            $(this).removeClass('active');
+
+            $('#'+panelToShow).slideDown(300, function() {
+                $(this).addClass('active');
+            });
+        }
+    });
+
+
+});
+
+
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+var slideIndex = 0;
+showSlidesbis();
+
+function showSlidesbis() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlidesbis, 5000);
+}
